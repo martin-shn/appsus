5e5308e8b7d636a5471ed06e948292ef4aaf75a8
Martin Sh <martinshn@gmail.com>
2021-08-27 17:10:50 +0300

    added link

--- js/apps/email/cmps/email-side-nav.jsx
+++ js/apps/email/cmps/email-side-nav.jsx
@@ -1,13 +1,14 @@
-export function EmailSideNav(){
+const { Link } = ReactRouterDOM;
 
-        return(
-            <section className="email-side-nav">
-                <Link to="/mail/">All</Link>
-                <Link to="/mail/inbox">Inbox</Link>
-                <Link to="">Starred</Link>
-                <Link to="/mail/sent">Sent</Link>
-                <Link to="/mail/drafts">Drafts</Link>
-            </section>
-        )
-    
+export function EmailSideNav() {
+
+    return (
+        <section className='email-side-nav'>
+            <Link to='/mail/'>All</Link>
+            <Link to='/mail/inbox'>Inbox</Link>
+            <Link to=''>Starred</Link>
+            <Link to='/mail/sent'>Sent</Link>
+            <Link to='/mail/drafts'>Drafts</Link>
+        </section>
+    );
 }
