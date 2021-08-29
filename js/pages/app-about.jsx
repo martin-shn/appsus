d49afc89e6ac1036e34ee1fcfc98beace5c104cc
Martin Sh <martinshn@gmail.com>
2021-08-29 06:44:37 +0300

    css

--- js/pages/app-about.jsx
+++ js/pages/app-about.jsx
@@ -27,7 +27,7 @@ export class About extends React.Component {
                   <p>Martin Sh</p>
           </div>                
         <div>
-          <img className="shaul" src="./css/img/shaul.jpg"></img>
+          <img className="shaul" src="./css/img/shaul.JPG"></img>
           <div className="socials">
                       <a href="linkedin.com/in/shaul-batzon-a65b98169/" target="_blank"></a>
                       <a href="https://www.facebook.com/shaul.batzon" target="_blank"></a>
